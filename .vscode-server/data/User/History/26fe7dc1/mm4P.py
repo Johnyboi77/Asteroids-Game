e55fe8b1b4ddb288c536abd4f7c6c88cfc6ab912
Johnnboi <email@example.com>
2024-11-21 11:46:10 +0100

    Player Size

--- .vscode-server/data/User/History/26fe7dc1/mm4P.py
+++ .vscode-server/data/User/History/26fe7dc1/mm4P.py
@@ -4,10 +4,6 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-    # Initialize clock here, before the game loop
-    clock = pygame.time.Clock()
-    dt = 0
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -16,15 +12,7 @@ def main():
     running = True
     while running:
         for event in pygame.event.get():
-            pass  # handle your events here
-            
-        # Draw your game here
-        
-        pygame.display.flip()
-        
-        # Control frame rate and get delta time at the end of each loop
-        dt = clock.tick(60) / 1000
-        
+            pygame.display.flip()
     pygame.quit()
 
 if __name__ == "__main__":
---
+++ .vscode-server/data/User/History/36aaad9/7fVE.py
@@ -0,0 +1,11 @@
+Class Player(CircleShape):
+    Player__init__(x, y)
+    super.__init__CircleShape
+
+def triangle(self):
+    forward = pygame.Vector2(0, 1).rotate(self.rotation)
+    right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+    a = self.position + forward * self.radius
+    b = self.position - forward * self.radius - right
+    c = self.position - forward * self.radius + right
+    return [a, b, c]
